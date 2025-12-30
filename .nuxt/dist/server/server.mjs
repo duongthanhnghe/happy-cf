cb7a54120b313a8c10a6341fb3562185be615752
duongthanhnghe <32519166+duongthanhnghe@users.noreply.github.com>
2025-12-30 11:46:53 +0700

    chore: remove .nuxt build artifacts from repository

--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/ttcenter/happy-cf/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
--- .nuxt/types/nitro-middleware.d.ts
+++
@@ -1,6 +0,0 @@
-export type MiddlewareKey = "admin-role" | "auth-login" | "category-news-detail" | "category-product-detail" | "news-main" | "post-detail" | "product-detail" | "product-most-order" | "product-sale" | "product-search-result"
-declare module 'nitropack' {
-  interface NitroRouteConfig {
-    appMiddleware?: MiddlewareKey | MiddlewareKey[] | Record<MiddlewareKey, boolean>
-  }
-}
